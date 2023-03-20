dc42a8408a9db860c465e2ed172b77b046730cb0
ZenT3600 <matteo.leggio@tiscali.it>
2023-03-20 14:59:21 +0100

    Bumped version

--- app/src/main/java/it/matteoleggio/seventhseadicer/ui/AboutActivity.java
+++ app/src/main/java/it/matteoleggio/seventhseadicer/ui/AboutActivity.java
@@ -17,7 +17,6 @@ public class AboutActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
 
-        ((TextView)findViewById(R.id.secusoWebsite)).setMovementMethod(LinkMovementMethod.getInstance());
         ((TextView)findViewById(R.id.githubURL)).setMovementMethod(LinkMovementMethod.getInstance());
         ((TextView)findViewById(R.id.textFieldVersion)).setText(getString(R.string.version_number, BuildConfig.VERSION_NAME));
 
